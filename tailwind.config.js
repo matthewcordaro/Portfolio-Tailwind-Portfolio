17bd7136abe77472fccc37e732775b1cf2c2f0a7
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-05-14 16:30:03 -0400

    Refactor tailwind.config.js to properly extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      'matt-blue':{
-        100: '#c5e5fc',
-        200: '#a5d5f8',
-        300: '#8bcbf9',
-        400: '#6ebef7',
-        500: '#49a6e9',
-        600: '#2d87c8',
-        700: '#1a6aa2',
-        800: '#104e7a',
-        900: '#063251',
-      }
+    extend: {
+      colors: {
+        "matt-blue": {
+          100: "#c5e5fc",
+          200: "#a5d5f8",
+          300: "#8bcbf9",
+          400: "#6ebef7",
+          500: "#49a6e9",
+          600: "#2d87c8",
+          700: "#1a6aa2",
+          800: "#104e7a",
+          900: "#063251",
+        },
+      },
     },
   },
   plugins: [],
