6751758bbd95d96f39072face12cddc205d92731
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-05-14 14:51:33 -0400

    Init Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import heroImg from "../assets/hero.svg"
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa"
+import React from "react"
+
+function Hero() {
+  return (
+    <>
+      <img src={heroImg} />
+    </>
+  )
+}
+
+export default Hero
