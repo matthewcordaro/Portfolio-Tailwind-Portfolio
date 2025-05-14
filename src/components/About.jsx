2a96618a5666db248a18b99322a90e1f3051b957
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-05-14 16:06:02 -0400

    Finish About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,19 @@ import React from "react"
 
 function About() {
   return (
-    <section id='about'>
-      <SectionTitle text='About' />
-      <img src={aboutSvg} />
+    <section className='bg-white py-20' id='about'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+        <img src={aboutSvg} className='w-full h-64' />
+        <article>
+          <SectionTitle text='About Matt' />
+          <p className='text-slate-600 mt-8 leading-loose'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
+            cupiditate consequatur provident voluptates nobis fugiat assumenda
+            beatae repellendus sequi non omnis corrupti, ut inventore eius ea
+            rerum? Vitae, repellat dolorem.
+          </p>
+        </article>
+      </div>
     </section>
   )
 }
