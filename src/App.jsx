febd5ed767c2731dfe76d283eadc4b8aea28c88e
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-05-14 15:54:51 -0400

    Add Skills section and related components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Skills from "./components/Skills"
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
     </>
   )
 }
